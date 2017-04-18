e8afce2800a1579643e9105186b68423daeeb8c6
Paul Kassar <your email>
2017-04-18 11:56:42 +0100

    step 4 completed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,14 @@ require "./lib/oystercard"
 
 describe Oystercard do
 
+
+  it 'has a balance' do
+    expect(subject).to respond_to :balance
+  end
+
+
+  it 'begins at a balance of 0' do
+    expect(subject.balance).to eq(0)
+  end
+
 end
